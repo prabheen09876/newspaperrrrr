b0c3326f4bed43f1738213201362ea2f3f4a7a55
Abhii <prabheen09876@gmail.com>
2024-01-15 13:49:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.addEventListener('load', () => {
+//     var name=localStorage.getItem("Name")
+//     document.getElementById('set_value').innerHTML="Name:"+name
+//     var imageData = localStorage.getItem('Image');
+//     if (imageData) {
+//         var imageElement = document.getElementById('profile');
+//         imageElement.src = imageData; 
+//     }
+// });
